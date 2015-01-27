d7dcb5a83be012e78f89ffd8a98049ad4f81cb7f
fuczak <prus.damian@gmail.com>
2015-01-27 12:45:47 +0100

    added todo methods to server.js and core.js init

--- server.js
+++ server.js
@@ -10,13 +10,64 @@ var methodOverride = require('method-override');	//simulate DELETE and PUT
 
 //configuration
 mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uw03mypu'); //connect to mongoDB databse on modulus.info
-
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));	//log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));	//parse application/x-www-form-urlencoded
 app.use(bodyParser.json());	//parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); //parse application/vnd.api+json as json
 app.use(methodOverride());
+
+//define todo model
+vat Todo = mongoose.model('Todo', {
+	text : String
+});
+
+//routes
+	//get all todos
+	app.get('/api/todos', function (req, res) {
+		//use mongoose to get all todos in the DB
+		Todo.find(function (err, todos) {
+			if(err)
+				res.send(err)
+			res.json(todos);
+		});
+	});
+	//create a todo and send back all todos after creation
+	app.post('/api/todos', function (req, res) {
+		//create a todo, info comes from AJAX request from Angular
+		Todo.create({
+			text : req.body.text,
+			done : false
+		}, function (err, todo) {
+			if(err)
+				res.send(err);
+			//get and return all the todos after creating one
+			Todo.find(function (err, todos) {
+				if(err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
+	//delete a todo
+	app.delete('/api/todos/:todo_is', function (req, res) {
+		Todo.remove({
+			_id : req.params.todo_id
+		}, function (err, todo) {
+			if(err)
+				res.sens(err);
+			//get and return all the todos after deleting one
+			Todo.find(function (err, todos) {
+				if(err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
+	//main route
+	app.get('*', function (req, res) {
+		res.sendfile('./public/index.html');
+	});
 
 //listen (start app with node server.js)
 app.listen(8080);
